3b8d2b39c16f35aec0e6e5c987dd2eefed98c805
Владимир Миронов <giligan2018@mail.ru>
2021-02-09 22:43:27 +0300

    Разделил проект на модули

---
+++ js/utils.js
@@ -0,0 +1,26 @@
+const getRandomInteger = (min, max) => {
+  if (max < min) {
+    return Math.round(Math.random() * (min - max) + max);
+  }
+  return Math.round(Math.random() * (max - min) + min);
+}
+
+const getRandomDigit = (minDigit, maxDigit, decimalPlaces) => {
+  let randomNumber;
+  if (maxDigit < minDigit) {
+    randomNumber = (Math.random() * (minDigit - maxDigit) + maxDigit);
+  }
+  randomNumber = (Math.random() * (maxDigit - minDigit) + minDigit);
+  return parseFloat(randomNumber.toFixed(decimalPlaces));
+}
+
+const getRandomArrayElement = (elements) => {
+  return elements[getRandomInteger(0, elements.length - 1)];
+}
+
+const getRandomLengthArray = (arrayTitle) => {
+  arrayTitle.length = (getRandomInteger(1, arrayTitle.length));
+  return arrayTitle.slice();
+}
+
+export {getRandomInteger, getRandomDigit, getRandomArrayElement, getRandomLengthArray};
