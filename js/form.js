b400bdd92230143f67922312a0a0064c325420dc
Владимир Миронов <giligan2018@mail.ru>
2021-02-19 21:31:10 +0300

    Реализовал фукнционал полей тип жилья и время заезда

---
+++ js/form.js
@@ -0,0 +1,42 @@
+import {MIN_ARRAY_PRICES} from './data.js';
+
+let timeIn = document.querySelector('#timein');
+let timeOut = document.querySelector('#timeout');
+let typeApartment = document.querySelector('#type');
+let price = document.querySelector('#price');
+
+if (typeApartment.selectedIndex === 1) {
+  price.placeholder = MIN_ARRAY_PRICES[1];
+}
+
+typeApartment.addEventListener('change', () => {
+  if (typeApartment.selectedIndex === 0) {
+    price.placeholder = MIN_ARRAY_PRICES[0];
+  }
+  if (typeApartment.selectedIndex === 1) {
+    price.placeholder = MIN_ARRAY_PRICES[1];
+  }
+  if (typeApartment.selectedIndex === 2) {
+    price.placeholder = MIN_ARRAY_PRICES[2];
+  }
+  if (typeApartment.selectedIndex === 3) {
+    price.placeholder = MIN_ARRAY_PRICES[3];
+  }
+});
+
+timeIn.addEventListener('change', () => {
+  let current = timeIn.selectedIndex;
+  timeOut.options[current].selected = true;
+});
+
+timeOut.addEventListener('change', () => {
+  let current = timeOut.selectedIndex;
+  timeIn.options[current].selected = true;
+});
+
+
+
+
+
+
+
