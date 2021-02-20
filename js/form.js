712e9c9cdb8fdd3059ec25f0258e89cef111bd18
Владимир Миронов <giligan2018@mail.ru>
2021-02-20 18:52:46 +0300

    Добавил атрибут min к цене

--- js/form.js
+++ js/form.js
@@ -7,20 +7,25 @@ let price = document.querySelector('#price');
 
 if (typeApartment.selectedIndex === 1) {
   price.placeholder = MIN_ARRAY_PRICES[1];
+  price.min = MIN_ARRAY_PRICES[1];
 }
 
 typeApartment.addEventListener('change', () => {
   if (typeApartment.selectedIndex === 0) {
     price.placeholder = MIN_ARRAY_PRICES[0];
+    price.min = MIN_ARRAY_PRICES[0];
   }
   if (typeApartment.selectedIndex === 1) {
     price.placeholder = MIN_ARRAY_PRICES[1];
+    price.min = MIN_ARRAY_PRICES[1];
   }
   if (typeApartment.selectedIndex === 2) {
     price.placeholder = MIN_ARRAY_PRICES[2];
+    price.min = MIN_ARRAY_PRICES[2];
   }
   if (typeApartment.selectedIndex === 3) {
     price.placeholder = MIN_ARRAY_PRICES[3];
+    price.min = MIN_ARRAY_PRICES[3];
   }
 });
 
