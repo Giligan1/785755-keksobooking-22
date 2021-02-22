358ef5673927e11df9363a764c53fe69746d7d70
Владимир Миронов <giligan2018@mail.ru>
2021-02-22 15:58:13 +0300

    Добавил валидацию формы

--- js/form.js
+++ js/form.js
@@ -1,9 +1,15 @@
 import {MIN_ARRAY_PRICES} from './data.js';
 
+const MIN_TITLE_LENGTH = 30;
+const MAX_TITLE_LEGNTH = 100;
+
 let timeIn = document.querySelector('#timein');
 let timeOut = document.querySelector('#timeout');
 let typeApartment = document.querySelector('#type');
 let price = document.querySelector('#price');
+let title = document.querySelector('#title');
+let numberOfRooms = document.querySelector('#room_number');
+let capacity = document.querySelector('#capacity');
 
 if (typeApartment.selectedIndex === 1) {
   price.placeholder = MIN_ARRAY_PRICES[1];
@@ -39,9 +45,56 @@ timeOut.addEventListener('change', () => {
   timeIn.options[current].selected = true;
 });
 
+title.addEventListener('input', () => {
+  const titleValue = title.value.length;
+  if (titleValue < MIN_TITLE_LENGTH) {
+    title.setCustomValidity(`Ещё ${MIN_TITLE_LENGTH - titleValue} симв.`)
+  } else if (titleValue > MAX_TITLE_LEGNTH) {
+    title.setCustomValidity(`Удалите лишние ${titleValue - MAX_TITLE_LEGNTH} симв.`)
+  } else {
+    title.setCustomValidity('');
+  }
 
+  title.reportValidity();
+});
 
+if (numberOfRooms.selectedIndex === 0) {
+  capacity.selectedIndex = 2;
+  capacity.options[0].setAttribute('disabled' ,'disabled');
+  capacity.options[1].setAttribute('disabled' ,'disabled');
+  capacity.options[3].setAttribute('disabled' ,'disabled');
+  capacity.setCustomValidity('Для 1 гостя');
+}
 
-
-
-
+numberOfRooms.addEventListener('change', () => {
+  if (numberOfRooms.selectedIndex === 0) {
+    capacity.selectedIndex = 2;
+    capacity.options[2].removeAttribute('disabled' ,'disabled');
+    capacity.options[0].setAttribute('disabled' ,'disabled');
+    capacity.options[1].setAttribute('disabled' ,'disabled');
+    capacity.options[3].setAttribute('disabled' ,'disabled');
+    capacity.setCustomValidity('Для 1 гостя');
+  } else if (numberOfRooms.selectedIndex === 1) {
+    capacity.selectedIndex = 2;
+    capacity.options[1].removeAttribute('disabled' ,'disabled');
+    capacity.options[2].removeAttribute('disabled' ,'disabled');
+    capacity.options[0].setAttribute('disabled' ,'disabled');
+    capacity.options[3].setAttribute('disabled' ,'disabled');
+    capacity.setCustomValidity('Для 2 гостей или для 1 гостя');
+  } else if (numberOfRooms.selectedIndex === 2) {
+    capacity.selectedIndex = 2;
+    capacity.options[0].removeAttribute('disabled' ,'disabled');
+    capacity.options[1].removeAttribute('disabled' ,'disabled');
+    capacity.options[2].removeAttribute('disabled' ,'disabled');
+    capacity.options[3].setAttribute('disabled' ,'disabled');
+    capacity.setCustomValidity('Для 3 гостей, для 2 гостей или для 1 гостя');
+  } else if (numberOfRooms.selectedIndex === 3) {
+    capacity.options[3].removeAttribute('disabled' ,'disabled');
+    capacity.options[0].setAttribute('disabled' ,'disabled');
+    capacity.options[1].setAttribute('disabled' ,'disabled');
+    capacity.options[2].setAttribute('disabled' ,'disabled');
+    capacity.selectedIndex = 3;
+    capacity.setCustomValidity('Не для гостей');
+  }
+  capacity.reportValidity();
+});
